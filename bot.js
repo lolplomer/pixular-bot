4566dd6ae5b86de1173044afab149153c7c41330
lolplomer <56777736+lolplomer@users.noreply.github.com>
2019-11-02 20:57:58 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,17 +3,30 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    bot.user.setStatus('available')
-    bot.user.setPresence({
-        game: {
-            name: 'pixularcraft.tk',
-            type: "LISTENING",
-            url: "https://pixularcraft.tk/"
 });
 
 client.on('message', message => {
     if (message.content === '!ip'){
         message.channel.send(`Hey ${message.author}, The IP is **pixularcraft.tk**`);
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '!help') {
+        const helpEmbed = new Discord.RichEmbed()
+            .setColor('#0099ff')
+            .setTitle('PixularCraft help')
+            .setURL('https://pixularcraft.tk/')
+            .setAuthor('Requested by ${message.author}')
+            .setDescription('Some helpful commands')
+            .setThumbnail('https://imgur.com/NNHSpWI')
+            .addField('!ip', 'Get the server ip')
+            .addField('!report', 'Report someone', true)
+            .addField('!new', 'Create ticket for support', true)
+            .setTimestamp()
+            .setFooter('PixularCraft bot by lolplomer#8123 :)', 'https://imgur.com/NNHSpWI');
+
+        channel.send(helpEmbed);
   	}
 });
 
