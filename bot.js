88aa55fb4f2b8a33fb68d57852e9dc25bf6ea68b
lolplomer <56777736+lolplomer@users.noreply.github.com>
2019-11-02 19:35:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,10 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === '#ip') {
+    	message.reply('The IP is **play.pixularcraft.tk**');
+  	}
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
